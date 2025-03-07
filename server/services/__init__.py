30dd3c583959053feae1ab45236fb1311ca451da
ath31st <ath31st@yandex.ru>
2025-03-07 21:48:54 +0300

    update init with services

--- server/services/__init__.py
+++ server/services/__init__.py
@@ -1,3 +1,5 @@
 from .user_service import UserService
+from .comment_service import CommentService
+from .issue_service import IssueService
 
-__all__ = ["UserService"]
+__all__ = ["UserService", "CommentService", "IssueService"]
