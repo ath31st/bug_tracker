eaafc2d353629004605f0107cb0d7c2b2a097812
ath31st <ath31st@yandex.ru>
2025-03-08 19:40:27 +0300

    update app py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config.config import Config
 from models import db
+from services import jwt
 from flask_bcrypt import Bcrypt
 from routes import register_routes
 
@@ -9,6 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    jwt.init_app(app)
     db.init_app(app)
     bcrypt = Bcrypt(app)
 
