25579943a49047b4fcd3e8313c2fb308dc39e815
ath31st <ath31st@yandex.ru>
2025-03-08 19:32:05 +0300

    update config.py with jwt secret keys

--- server/config/config.py
+++ server/config/config.py
@@ -8,3 +8,8 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_TOKEN_LOCATION = ["headers"]
+    JWT_HEADER_NAME = "Authorization"
+    JWT_HEADER_TYPE = "Bearer"
