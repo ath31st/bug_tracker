8f962da58c98eb7ec7ab981804030fb5f51d0ebe
ath31st <ath31st@yandex.ru>
2025-03-11 13:55:29 +0300

    add types (user, auth, issue, comment)

---
+++ client/src/types/index.ts
@@ -0,0 +1,79 @@
+// auth types
+
+export interface Credentials {
+  username: string;
+  password: string;
+}
+
+export interface JwtResponse {
+  accessToken: string;
+}
+
+// user types
+
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+}
+
+export interface NewUser {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface UpdateUser {
+  username?: string;
+  email?: string;
+  password?: string;
+}
+
+// issue types
+
+export interface Issue {
+  id: number;
+  title: string;
+  description: string;
+  status: string;
+  priority: string;
+  reporter: User;
+  assignee?: User;
+  comments: Comment[];
+  created_at: string;
+  updated_at: string;
+}
+
+export interface NewIssue {
+  title: string;
+  description: string;
+  priority: string;
+}
+
+export interface UpdateIssue {
+  title?: string;
+  description?: string;
+  status?: string;
+  priority?: string;
+  assignee_id?: number;
+}
+
+// comment types
+
+export interface Comment {
+  id: number;
+  content: string;
+  issue: Issue;
+  created_at: string;
+  updated_at: string;
+  author: User;
+}
+
+export interface NewComment {
+  content: string;
+  issue_id: number;
+}
+
+export interface UpdateComment {
+  content?: string;
+}
