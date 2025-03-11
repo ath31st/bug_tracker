30f7c8de6d8c9e6afd7c9a22f70957bb870aaffe
ath31st <ath31st@yandex.ru>
2025-03-11 15:29:13 +0300

    update auth store

---
+++ client/src/stores/authStore.ts
@@ -0,0 +1,36 @@
+import type { Credentials } from '@/types';
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import { login } from '@/api/authApi';
+import { decodeToken } from '@/api/jwtApi';
+import type { JwtUser } from '@/types';
+
+export const useAuthStore = defineStore('auth', () => {
+  const token = ref<string | null>(null);
+  const user = ref<JwtUser | null>(null);
+
+  async function loginUser(credentials: Credentials) {
+    try {
+      const response = await login(credentials);
+
+      if (response && response.accessToken) {
+        const accessToken = response.accessToken;
+        localStorage.setItem('token', accessToken);
+        token.value = accessToken;
+        user.value = decodeToken(accessToken);
+        return { token: token.value, user: user.value };
+      }
+
+      return { token: null, user: null };
+    } catch (error) {
+      console.error('Login failed:', error);
+      return { token: null, user: null };
+    }
+  }
+
+  return {
+    token,
+    user,
+    loginUser,
+  };
+});
