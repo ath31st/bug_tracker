1cf4b6b912dcfdacdf836203640372046aeae2ae
ath31st <ath31st@yandex.ru>
2025-03-11 19:00:31 +0300

    update routes with login page and check for auth users

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,8 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import LoginView from '@/views/LoginView.vue';
+import { useAuthStore } from '@/stores/authStore';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+  ],
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+
+  if (to.meta.requiresAuth && !authStore.user) {
+    next('/login');
+  } else {
+    next();
+  }
+});
+
+export default router;
