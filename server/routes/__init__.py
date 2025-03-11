2f30fdad90688a9bd57ca97a76d890c4528f9170
ath31st <ath31st@yandex.ru>
2025-03-11 15:14:20 +0300

    add humps for camelize data after requests from server

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,3 +1,5 @@
+from flask import jsonify
+from humps import camelize
 from .user_routes import create_user_routes
 from .issue_routes import create_issue_routes
 from .comment_routes import create_comment_routes
@@ -8,6 +10,15 @@ from flask_bcrypt import Bcrypt
 def register_routes(
     app, bcrypt: Bcrypt, user_service, comment_service, issue_service, auth_service
 ):
+
+    @app.after_request
+    def camelize_response(response):
+        if response.content_type == "application/json":
+            data = response.get_json()
+            if data is None:
+                response.set_data(jsonify(camelize(data)).data)
+        return response
+
     app.register_blueprint(create_user_routes(bcrypt, user_service))
     app.register_blueprint(create_issue_routes(issue_service))
     app.register_blueprint(create_comment_routes(comment_service))
