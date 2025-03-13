27a15340d48964df99524d52623793085493afc9
ath31st <ath31st@yandex.ru>
2025-03-13 22:29:08 +0300

    update comment schema

--- server/validations/comment/comment_schema.py
+++ server/validations/comment/comment_schema.py
@@ -5,6 +5,6 @@ class CommentSchema(Schema):
     id = fields.Int()
     content = fields.Str()
     issue_id = fields.Int()
-    author_id = fields.Int()
+    author = fields.Nested("UserSchema")
     created_at = fields.DateTime()
     updated_at = fields.DateTime()
