596088eeeb56b4a447578371ab89908c75d2accf
ath31st <ath31st@yandex.ru>
2025-03-13 16:01:17 +0300

    add comment item component

---
+++ client/src/components/comment/CommentItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-list-item>
+    <v-list-item-title>
+      <strong>{{ comment.author.username }}</strong>
+      <span class="comment-date">{{ formatDate(comment.createdAt) }}</span>
+    </v-list-item-title>
+    <v-list-item-subtitle>{{ comment.content }}</v-list-item-subtitle>
+  </v-list-item>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { formatDate } from '@/utils/formatDate';
+import type { Comment } from '@/types';
+
+defineProps<{
+  comment: Comment;
+}>();
+</script>
+
+<style scoped>
+.comment-date {
+  font-size: 0.8rem;
+  color: gray;
+  margin-left: 8px;
+}
+</style>
