c56afcb85484b74810c8e3a724ca358264efd5b3
ath31st <ath31st@yandex.ru>
2025-03-13 14:23:06 +0300

    update main header

--- client/src/components/HeaderComponent.vue
+++ client/src/components/HeaderComponent.vue
@@ -5,18 +5,17 @@
     </v-toolbar-title>
     <v-spacer></v-spacer>
 
-    <v-btn text to="/" exact>Home</v-btn>
-    <v-btn text to="/about">About</v-btn>
+    <div class="d-flex">
+      <v-btn text to="/" exact>Главная</v-btn>
+      <v-btn text to="/about">О проекте</v-btn>
 
-    <template v-if="authStore.user">
-      <v-btn text @click="logout">Logout</v-btn>
-      <v-chip size="large" class="ml-2 mr-10">
-        {{ authStore.user?.username.toUpperCase() || 'User' }}
-      </v-chip>
-    </template>
-    <template v-else>
-      <v-btn text to="/login">Login</v-btn>
-    </template>
+      <template v-if="authStore.user">
+        <v-btn text @click="logout">Выход</v-btn>
+        <v-chip size="large" class="ml-2 mr-10">
+          {{ authStore.user?.username.toUpperCase() || 'User' }}
+        </v-chip>
+      </template>
+    </div>
   </v-app-bar>
 </template>
 
@@ -34,6 +33,9 @@ function logout() {
 </script>
 
 <style scoped>
+.d-flex > * {
+  margin-right: 16px;
+}
 .v-btn {
   width: 100px;
 }
