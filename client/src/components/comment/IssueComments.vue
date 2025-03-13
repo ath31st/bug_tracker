0c6d51ba6e9ae454c1c5d42c0774726ed9576d0e
ath31st <ath31st@yandex.ru>
2025-03-13 16:02:26 +0300

    add issue comment list component

---
+++ client/src/components/comment/IssueComments.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h3>Комментарии</h3>
+
+    <v-list v-if="comments.length">
+      <CommentItem
+        v-for="comment in comments"
+        :key="comment.id"
+        :comment="comment"
+      />
+    </v-list>
+
+    <v-alert v-else type="info" class="mt-2">Комментариев пока нет</v-alert>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import type { Comment } from '@/types';
+
+defineProps<{
+  comments: Comment[];
+}>();
+</script>
+
+<style scoped>
+.comment-date {
+  font-size: 0.8rem;
+  color: gray;
+  margin-left: 8px;
+}
+</style>
