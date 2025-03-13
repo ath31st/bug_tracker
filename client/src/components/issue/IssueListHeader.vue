152e61f4a6780bf0d412604cc8368413f2ab67f7
ath31st <ath31st@yandex.ru>
2025-03-13 14:17:02 +0300

    add issues list header

---
+++ client/src/components/issue/IssueListHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row>
+    <v-col cols="1" class="header-col">
+      <span>ID</span>
+    </v-col>
+    <v-col cols="2" class="header-col">
+      <span>Заголовок</span>
+    </v-col>
+    <v-col cols="1" class="header-col">
+      <span>Статус</span>
+    </v-col>
+    <v-col cols="1" class="header-col">
+      <span>Приоритет</span>
+    </v-col>
+    <v-col cols="2" class="header-col">
+      <span>Автор</span>
+    </v-col>
+    <v-col cols="2" class="header-col">
+      <span>Исполнитель</span>
+    </v-col>
+    <v-col cols="3" class="header-col">
+      <span>Дата создания</span>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.header-col {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+}
+</style>
