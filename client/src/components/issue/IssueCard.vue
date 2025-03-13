15fa97fc3f9c12299403ea8f375d7e0e21641b5d
ath31st <ath31st@yandex.ru>
2025-03-13 14:23:54 +0300

    add issue card component

---
+++ client/src/components/issue/IssueCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <v-row align="center">
+        <v-col cols="1">
+          <span class="font-weight-bold">#{{ issue.id }}</span>
+        </v-col>
+        <v-col cols="2">
+          <span>{{ issue.title }}</span>
+        </v-col>
+        <v-col cols="1" class="d-flex justify-center align-center">
+          <v-chip :color="getStatusColor(issue.status)" size="small">
+            {{ getStatusName(issue.status) }}
+          </v-chip>
+        </v-col>
+        <v-col cols="1" class="d-flex justify-center align-center">
+          <v-chip :color="getPriorityColor(issue.priority)" size="small">
+            {{ getPriorityName(issue.priority) }}
+          </v-chip>
+        </v-col>
+        <v-col cols="2">
+          <span>{{ issue.reporter.username }}</span>
+        </v-col>
+        <v-col cols="2">
+          <span>{{ issue.assignee?.username || '-' }}</span>
+        </v-col>
+        <v-col cols="3">
+          <span>{{ formatDate(issue.createdAt) }}</span>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import type { Issue } from '@/types';
+import { formatDate } from '@/utils/formatDate';
+import { getStatusColor, getStatusName } from '@/utils/statusUtils';
+import { getPriorityColor, getPriorityName } from '@/utils/priorityUtils';
+
+defineProps<{
+  issue: Issue;
+}>();
+</script>
+
+<style scoped></style>
