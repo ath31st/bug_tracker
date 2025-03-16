ea9b095e62a406734a05bdaa11d25ca2d71bebcf
ath31st <ath31st@yandex.ru>
2025-03-16 11:43:26 +0300

    clear issue view

--- client/src/views/IssueView.vue
+++ client/src/views/IssueView.vue
@@ -1,27 +1,14 @@
 <template>
   <v-container>
-    <SpinnerLoader v-if="issuesStore.loading" />
-    <IssueCardDetailed v-if="issue" :issue="issue" />
+    <IssueCardDetailed :issueId="issueId" />
   </v-container>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 import { useRoute } from 'vue-router';
-import { useIssuesStore } from '@/stores/issueStore';
-import SpinnerLoader from '@/components/loader/SpinnerLoader.vue';
 import IssueCardDetailed from '@/components/issue/IssueCardDetailed.vue';
 
 const route = useRoute();
-const issuesStore = useIssuesStore();
-const issue = ref();
-
-onMounted(async () => {
-  const issueId = Number(route.params.id);
-  try {
-    issue.value = await issuesStore.fetchIssue(issueId);
-  } catch (error) {
-    console.error('Error loading issue:', error);
-  }
-});
+const issueId = ref(Number(route.params.id));
 </script>
