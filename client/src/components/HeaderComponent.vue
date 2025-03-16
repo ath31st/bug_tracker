f2988a8cc5e49da785d451d67926a0a76f554046
ath31st <ath31st@yandex.ru>
2025-03-16 13:07:42 +0300

    update header with user info modal

--- client/src/components/HeaderComponent.vue
+++ client/src/components/HeaderComponent.vue
@@ -11,25 +11,64 @@
 
       <template v-if="authStore.user">
         <v-btn text @click="logout">Выход</v-btn>
-        <v-chip size="large" class="ml-2 mr-10">
+        <v-btn @click="openUserModal">
           {{ authStore.user?.username.toUpperCase() || 'User' }}
-        </v-chip>
+        </v-btn>
       </template>
     </div>
   </v-app-bar>
+
+  <UserModal
+    :user="selectedUser"
+    :visible="isModalVisible"
+    @close="closeUserModal"
+  />
 </template>
 
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/authStore';
+import { useUsersStore } from '@/stores/userStore';
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import UserModal from '@/components/modal/UserModal.vue';
+import type { User } from '@/types';
 
 const authStore = useAuthStore();
+const usersStore = useUsersStore();
 const router = useRouter();
+
+const isModalVisible = ref(false);
+const selectedUser = ref<User | null>(null);
 
 function logout() {
   authStore.logout();
   router.push('/login');
 }
+
+const fetchUser = async (userId: number) => {
+  try {
+    const user = await usersStore.fetchUser(userId);
+    return user;
+  } catch (error) {
+    console.error('Ошибка при загрузке пользователя:', error);
+    return null;
+  }
+};
+
+const openUserModal = async () => {
+  if (authStore.user?.id) {
+    const user = await fetchUser(authStore.user.id);
+    if (user) {
+      selectedUser.value = user;
+      isModalVisible.value = true;
+    }
+  }
+};
+
+const closeUserModal = () => {
+  isModalVisible.value = false;
+  selectedUser.value = null;
+};
 </script>
 
 <style scoped>
