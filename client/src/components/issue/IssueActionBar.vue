7a15a746b1a6ec56f5d3d2495b87e9fffa8a4407
ath31st <ath31st@yandex.ru>
2025-03-16 16:51:22 +0300

    add issue action bar

---
+++ client/src/components/issue/IssueActionBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card-actions>
+    <v-row>
+      <v-col>
+        <CommonButton variant="outlined" to="/new-issue">
+          <v-icon>mdi-bug</v-icon>
+          Создать
+        </CommonButton>
+      </v-col>
+    </v-row>
+  </v-card-actions>
+</template>
+
+<script setup lang="ts">
+import CommonButton from '@/components/button/CommonButton.vue';
+</script>
