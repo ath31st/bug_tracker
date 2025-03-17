3b2469fc05a7609404eb3efd4daf77462cfc364f
ath31st <ath31st@yandex.ru>
2025-03-18 00:02:41 +0300

    fix path in auth api

--- client/src/api/authApi.ts
+++ client/src/api/authApi.ts
@@ -1,9 +1,9 @@
 import type { Credentials, JwtResponse } from '@/types';
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_PUBLIC_API_URL + '/auth';
+const API_URL = import.meta.env.VITE_PUBLIC_API_URL + '/auth/';
 
 export const login = async (credentials: Credentials): Promise<JwtResponse> => {
-  const response = await axios.post(`${API_URL}/login`, credentials);
+  const response = await axios.post(`${API_URL}login`, credentials);
   return response.data;
 };
